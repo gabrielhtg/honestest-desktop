f841fa9689ef2c021aab87c32d5b9b858f252d13
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2025-01-20 12:25:33 +0700

    menambah fitur

--- src/pages/CheckReadiness.tsx
+++ src/pages/CheckReadiness.tsx
@@ -3,15 +3,24 @@ import Webcam from 'react-webcam';
 import { Button } from '@/components/ui/button.tsx';
 import { ArrowLeft } from 'lucide-react';
 import { useNavigate } from 'react-router';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select';
 
 export default function CheckReadiness() {
-  // const [deviceId, setDeviceId] = useState<any>({});
+  const [deviceId, setDeviceId] = useState<any>({});
   const [devices, setDevices] = useState<any>([]);
   const navigate = useNavigate();
 
   const handleDevices = useCallback(
-    (mediaDevices: any) =>
-      setDevices(mediaDevices.filter(({ kind }: any) => kind === 'videoinput')),
+    (mediaDevices: any) => {
+      setDevices(mediaDevices.filter(({ kind }: any) => kind === 'videoinput'));
+      setDeviceId(mediaDevices.filter(({ kind }: any) => kind === 'videoinput')[0]);
+    },
     [setDevices]
   );
 
@@ -23,18 +32,23 @@ export default function CheckReadiness() {
     <div className={'flex flex-col h-screen w-screen items-center justify-center gap-5'}>
       <h1 className={'font-bold text-3xl'}>Check Readiness Page</h1>
 
-      {devices.map((device: any, key: any) => (
-        <div>
-          <Webcam
-            className={'-scale-x-100'}
-            audio={false}
-            videoConstraints={{ deviceId: device.deviceId }}
-          />
-          <div className={'mt-3'}>
-            <span className={'font-bold'}>Camera : </span> {device.label || `Device ${key + 1}`}
-          </div>
-        </div>
-      ))}
+      <Webcam className={'-scale-x-100'} audio={false} videoConstraints={deviceId.deviceId} />
+
+      <div className={'flex gap-3 items-center'}>
+        <span className={'font-bold'}>Camera : </span>
+        <Select value={deviceId.deviceId}>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select Camera" />
+          </SelectTrigger>
+          <SelectContent>
+            {devices.map((device: any, key: any) => (
+              <SelectItem value={device.deviceId}>
+                {device.label.trim() || `Device ${key + 1}`}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
 
       <Button
         onClick={() => {
